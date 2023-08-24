1751ecef0c074a2ad61f9810b887af918f306b31
malini <maliniraj9025565842@gmail.com>
2023-08-24 11:01:30 +0530

    first commit

---
+++ day2.js
@@ -0,0 +1,57 @@
+/*var age=prompt("enter the number");
+console.log(age)
+console.log(typeof(age))
+confirm("confirm")*/
+
+//"TO KNOW THE VARIABLE TYPE"
+/*var a="50";
+console.log(typeof(a))
+var b=40;
+console.log(typeof(b))*/
+
+//"TO CHANGE THE VARIABLE TO OTHER DATA TYPE"
+/*var a="50";
+console.log(typeof(a))
+console.log(typeof(Number(a)))*/
+
+//"ARITHMETIC OPERATIONS"
+/*var b="6"/"3";
+console.log(b)
+
+var c=10
+c+=20
+console.log(c)
+
+var d=3**2
+console.log(d)*/
+
+
+//"CONCATENATION"
+/*var a=34
+var b=55
+var c="yu"
+console.log(a+b+c)
+
+var d="iu"
+var e=78
+var f=90
+console.log(d+e+f)*/
+
+
+//"FUNCTION"
+
+/*function add(a,b){
+    console.log(a+b)
+}
+add(7,9)*/
+
+//"CONDITIONAL OPERATOR"
+/*var a=10
+var b=45
+const c=(a>b)?"a is greatest":"b is greatest"
+console.log(c)*/
+
+
+
+
+
---
+++ day3.js
@@ -0,0 +1,73 @@
+//OBJECT
+/*let obj1={
+    name:"malini",
+    age:21
+}
+console.log(obj1.age)*/
+
+//NESTED OBJECT
+/*let obj1={
+    name:"malini",
+    age:21,
+    obj2:{
+        mark:70
+    }
+}
+console.log(obj1.obj2.mark)*/
+
+//objects with function
+/*let obj1={
+    name:"malini",
+    age:21,
+    obj2:{
+        mark:70
+    },
+    greet:function(a=6,b=5){
+        return a+b;
+    }
+}
+console.log(obj1.greet())*/
+
+//TO CHANGE THE VARIABLE IN OBJECT
+/*let obj1={
+    name:"malini",
+    age:21,
+    obj2:{
+        mark:70
+    }
+}
+obj1.age=10;
+obj1.obj2.mark=90;
+console.log(obj1.age)
+console.log(obj1.obj2.mark)*/
+
+//TO ADD NEW PROPERT IN OBJECT
+/*let obj1={
+    name:"malini",
+    age:21,
+    obj2:{
+        mark:70
+    }
+}
+obj1.num=12345;
+console.log(obj1.num)*/
+
+//TO DELETE A PROPERTY IN OBJECT
+/*let obj1={
+    name:"malini",
+    age:21,
+    obj2:{
+        mark:70
+    }
+}
+delete obj1.age;
+console.log(obj1.age)*/
+
+//CONSTRUCTOR
+
+
+
+
+
+
+
